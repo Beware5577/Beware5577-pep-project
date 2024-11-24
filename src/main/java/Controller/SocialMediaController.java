65c7994a1d925c9a30752ae59cf03b2e67f67a19
Sean Hansen-Busby <seanmhb@outlook.com>
2024-11-24 21:15:45 +0000

    Added - DAO Skeleton

--- src/main/java/Controller/SocialMediaController.java
+++ src/main/java/Controller/SocialMediaController.java
@@ -1,5 +1,7 @@
 package Controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+
 import io.javalin.Javalin;
 import io.javalin.http.Context;
 
@@ -16,18 +18,95 @@ public class SocialMediaController {
      */
     public Javalin startAPI() {
         Javalin app = Javalin.create();
-        app.get("example-endpoint", this::exampleHandler);
+        
+        //Endpoints
+        app.post("/register", this::postRegistrationHandler);
+        app.post("/login", this::postLoginHandler);
+        app.post("/messages", this::postMessagesHandler);
+        app.get("/messages", this::getAllMessagesHandler);
+        app.get("/messages/{message_id}", this::getMessageIdHandler);
+        app.delete("/messages/{message_id}", this::deleteMessageIdHandler);
+        app.patch("/messages/{message_id}", this::patchMessageIdHandler);
+        app.get("/accounts/{account_id}/messages", this::getAccountMessagesHandler);
+
+        app.start();
 
         return app;
     }
 
     /**
-     * This is an example handler for an example endpoint.
+     * Handler to post a new registration.
+     * 
      * @param context The Javalin Context object manages information about both the HTTP request and response.
+     * 
+     * @throws JsonProcessingException will be thrown if there is an issue converting JSON into an object.
      */
-    private void exampleHandler(Context context) {
+    private void postRegistrationHandler(Context context) throws JsonProcessingException{
         context.json("sample text");
     }
 
+
+    /**
+     * Handler to post a new login.
+     * 
+     * @param context The Javalin Context object manages information about both the HTTP request and response.
+     * 
+     * @throws JsonProcessingException will be thrown if there is an issue converting JSON into an object.
+     */
+    private void postLoginHandler(Context context) throws JsonProcessingException{} 
+
+    /**
+     * Handler to post a message.
+     * 
+     * @param context The Javalin Context object manages information about both the HTTP request and response.
+     * 
+     * @throws JsonProcessingException will be thrown if there is an issue converting JSON into an object.
+     */
+    private void postMessagesHandler(Context context) throws JsonProcessingException{}
+
+    /**
+     * Handler to get all messages.
+     * 
+     * @param context The Javalin Context object manages information about both the HTTP request and response.
+     * 
+     * @throws JsonProcessingException will be thrown if there is an issue converting JSON into an object.
+     */
+    private void getAllMessagesHandler(Context context) throws JsonProcessingException{}
+
+    /**
+     * Handler to get a message by a message ID.
+     * 
+     * @param context The Javalin Context object manages information about both the HTTP request and response.
+     * 
+     * @throws JsonProcessingException will be thrown if there is an issue converting JSON into an object.
+     */
+    private void getMessageIdHandler(Context context) throws JsonProcessingException{}
+
+    /**
+     * Handler to delete a message by a message ID.
+     * 
+     * @param context The Javalin Context object manages information about both the HTTP request and response.
+     * 
+     * @throws JsonProcessingException will be thrown if there is an issue converting JSON into an object.
+     */
+    private void deleteMessageIdHandler(Context context) throws JsonProcessingException{}
+
+    /**
+     * Handler to patch a message by a message ID.
+     * 
+     * @param context The Javalin Context object manages information about both the HTTP request and response.
+     * 
+     * @throws JsonProcessingException will be thrown if there is an issue converting JSON into an object.
+     */
+    private void patchMessageIdHandler(Context context) throws JsonProcessingException{}
+
+    /**
+     * Handler to get all messages from an account.
+     * 
+     * @param context The Javalin Context object manages information about both the HTTP request and response.
+     * 
+     * @throws JsonProcessingException will be thrown if there is an issue converting JSON into an object.
+     */
+    private void getAccountMessagesHandler(Context context) throws JsonProcessingException{}
 
 }
