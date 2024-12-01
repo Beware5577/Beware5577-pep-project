cd6ae2521949b87d36c446d2fd889dfd948a522a
Sean Hansen-Busby <seanmhb@outlook.com>
2024-12-01 21:27:51 +0000

    Added - Account DAO Skeleton

---
+++ src/main/java/DAO/AccountDAO.java
@@ -0,0 +1,21 @@
+package DAO;
+
+import Model.Account;
+
+public class AccountDAO {
+
+public boolean validAccountParameters(String username, String password)
+{
+    return false;
+}
+    
+public Account registerAccount(Account account)
+{
+    return null;
+}
+
+public Account logInAccount(String username, String password)
+{
+    return null;
+}
+}
