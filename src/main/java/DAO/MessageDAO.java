bf26ef6f2c3f8f95c0b02f300c06533ea86cd6e2
Sean Hansen-Busby <seanmhb@outlook.com>
2024-12-01 21:37:15 +0000

    Added - Message DAO Skeleton

---
+++ src/main/java/DAO/MessageDAO.java
@@ -0,0 +1,44 @@
+package DAO;
+
+import java.util.List;
+
+import Model.Message;
+
+public class MessageDAO {
+    
+public boolean validMessageParameters(String messageText, int accountId)
+{
+    return false;
+}
+
+public Message createMessage(Message message)
+{
+    return null;
+}
+
+public List<Message> getAllMessages()
+{
+    return null;
+}
+
+public Message getMessageById(int messageId)
+{
+    return null;
+}
+
+public Message deleteMessageById(int messageId)
+{
+    return null;
+}
+
+public Message patchMessageById(int messageId)
+{
+    return null;
+}
+
+public List<Message> getAllAccountMessages(int accountId)
+{
+    return null;
+}
+
+}
